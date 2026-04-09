02ecfbef5655e92431e883a28ba63c88ea65bf5a
tshi-github <ebideit360@gmail.com>
2026-04-09 13:41:36 +0900

    fix: update default port to 300 and remove host specification in app.listen

--- bot/server.js
+++ bot/server.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 300
 
 app.get('/', (req, res) => {
   res.send('OK')
 })
 
-app.listen(port, "0.0.0.0", () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
