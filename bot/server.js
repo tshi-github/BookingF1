515d05f8c95c0634c04410d95fbd04f20bba1d8f
tshi-github <ebideit360@gmail.com>
2026-04-09 12:38:49 +0900

    feat: add server.js with basic Express setup and dotenv configuration

---
+++ bot/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+  res.send('OK')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+require("./main")
